0c863f2d0f78889498f3f9bbb7dc905bf9258e94
Ben Furfie <benfurfie@bens-mac-mini.home>
2017-12-30 09:00:03 +0000

    Update version numbers.

--- remove-wordpress-comments.php
+++ remove-wordpress-comments.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package remove-wordpress-comments
- * @version 0.1.1
+ * @version 0.1.2
  */
 
 /*
   Plugin Name: Remove WordPress Comments
   Author: Ben Furfie
   Author URI: http://www.benfurfie.co.uk
-  Version: 0.1.0
+  Version: 0.1.2
 */
 
 defined('ABSPATH') or die('You cannot access this page directly.');
